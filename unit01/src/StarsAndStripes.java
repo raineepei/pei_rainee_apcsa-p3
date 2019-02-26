6a27f9d6eb05370025144757a4fa37efced7d4a5
Rainee Pei <pei.rainee@gmail.com>
2019-02-25 16:17:52 -0800

    unit01
    unit01

---
+++ unit01/src/StarsAndStripes.java
@@ -0,0 +1,40 @@
+//� A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsAndStripes
+{
+   public StarsAndStripes()
+   {
+      out.println("StarsAndStripes\n");
+   }
+
+   public void printTwentyStars()
+   {
+	   out.println("********************");
+   }
+
+   public void printTwentyDashes()
+   {
+	   out.println("--------------------");
+   }
+
+   public void printTwoBlankLines()
+   {
+	   out.println("               \n              ");
+   }
+   
+   public void printASmallBox()
+   {	
+   }
+ 
+   public void printABigBox()
+   { 	
+   }   
+}
---
+++ unit01/src/StarsRunner.java
@@ -0,0 +1,35 @@
+//� A+ Computer Science
+
+//www.apluscompsci.com
+
+//Name - Rainee Pei
+//Date - 02/01/19
+//Class - APCSA
+//Lab  - Stars and Stripes
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+      //instantiate a StarsAndStripes object
+      StarsAndStripes ref = new StarsAndStripes();
+      //call the methods needed to make the patterns on the word document
+      ref.printTwentyStars();
+      ref.printTwentyDashes();
+      ref.printTwentyDashes();
+      ref.printTwentyDashes();
+      ref.printTwentyStars();
+      ref.printTwentyStars();
+      ref.printTwentyDashes();
+      ref.printTwoBlankLines();
+      ref.printTwentyDashes();
+      ref.printTwentyDashes();
+      ref.printTwentyStars();
+      ref.printTwentyDashes();
+      ref.printTwentyStars();
+      ref.printASmallBox();
+      ref.printABigBox();
+   }
+}
