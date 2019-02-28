4ea69e001424bd3ff04147fc3d6af7a05705ce41
Rainee Pei <pei.rainee@gmail.com>
2019-02-27 20:37:30 -0800

    unit08
    unit08 labs

--- unit08/src/RayDown.java
+++ unit08/src/RayDown.java
@@ -9,6 +9,13 @@ public class RayDown
 	//are in decreasing order [31,12,6,2,1]
 	public static boolean go(int[] numArray)
 	{
-		return true;
+		for (int i = 0; i < numArray.length - 1; i++)
+		{
+			if (numArray[i + 1] >= numArray[i])
+			{
+				return false ;
+			}
+		}
+		return true;	
 	}	
 }
