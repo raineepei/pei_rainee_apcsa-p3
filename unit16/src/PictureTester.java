f27486d641dff1a005a50c1e7b0f326d6b477295
Rainee Pei <pei.rainee@gmail.com>
2019-04-24 22:42:49 -0700

    unit16
    unit16 pixLab

---
+++ unit16/src/PictureTester.java
@@ -0,0 +1,151 @@
+/**
+ * This class contains class (static) methods
+ * that will help you test the Picture class 
+ * methods.  Uncomment the methods and the code
+ * in the main to test.
+ * 
+ * @author Barbara Ericson 
+ */
+public class PictureTester
+{
+  /** Method to test zeroBlue */
+  public static void testZeroBlue()
+  {
+    Picture beach = new Picture("Users/raineepei/Downloads/Unit16Students/Unit16-Assignments-pixLab/images/beach.jpg");
+    beach.explore();
+    beach.zeroBlue();
+    beach.explore();
+  }
+  
+  public static void testKeepOnlyBlue() {
+	  Picture beach = new Picture("/Users/raineepei/Downloads/Unit16Students/Unit16-Assignments-pixLab/images/beach.jpg");
+	  beach.explore();
+	  beach.keepOnlyBlue();
+	  beach.explore();
+  }
+  
+  public static void testNegate() {
+	  Picture beach = new Picture("/Users/raineepei/Downloads/Unit16Students/Unit16-Assignments-pixLab/images/beach.jpg");
+	  beach.explore();
+	  beach.negate();
+	  beach.explore();
+  }
+  
+  public static void testGrayscale() {
+	  Picture beach = new Picture("/Users/raineepei/Downloads/Unit16Students/Unit16-Assignments-pixLab/images/beach.jpg");
+	  beach.explore();
+	  beach.grayscale();
+	  beach.explore();
+  }
+  /** Method to test mirrorVertical */
+  public static void testMirrorVertical()
+  {
+    Picture caterpillar = new Picture("/Users/raineepei/Downloads/Unit16Students/Unit16-Assignments-pixLab/images/caterpillar.jpg");
+    caterpillar.explore();
+    caterpillar.mirrorVerticalRightToLeft();
+    caterpillar.explore();
+  }
+  
+  public static void testMirrorHorizontal() 
+  {
+	  Picture motorcycle = new Picture("/Users/raineepei/Downloads/Unit16Students/Unit16-Assignments-pixLab/images/redMotorcycle.jpg");
+	  motorcycle.explore();
+	  motorcycle.mirrorHorizontal();
+	  motorcycle.explore();
+  }
+  
+  public static void testWater() {
+	  Picture water = new Picture("/Users/raineepei/Downloads/Unit16Students/Unit16-Assignments-pixLab/images/water.jpg");
+	  water.explore();
+	  water.fixUnderwater();
+	  water.explore();
+  }
+  
+  
+  /** Method to test mirrorTemple */
+  public static void testMirrorTemple()
+  {
+    Picture temple = new Picture("/Users/raineepei/Downloads/Unit16Students/Unit16-Assignments-pixLab/images/temple.jpg");
+    temple.explore();
+    temple.mirrorTemple();
+    temple.explore();
+  }
+  
+  public static void testMirrorArms() {
+	  Picture snowman = new Picture("/Users/raineepei/Downloads/Unit16Students/Unit16-Assignments-pixLab/images/snowman.jpg");
+	  snowman.explore();
+	  snowman.mirrorArms();
+	  snowman.explore();
+  }
+  
+  public static void testGull() {
+	  Picture gulls = new Picture("/Users/raineepei/Downloads/Unit16Students/Unit16-Assignments-pixLab/images/seagull.jpg");
+	  gulls.explore();
+	  gulls.mirrorGull();
+	  gulls.explore();
+  }
+  
+  /** Method to test the collage method */
+  public static void testCollage()
+  {
+    Picture canvas = new Picture("/Users/raineepei/Downloads/Unit16Students/Unit16-Assignments-pixLab/images/640x480.jpg");
+    canvas.createCollage();
+    canvas.explore();
+  }
+  
+  public static void testCopy()
+  {
+	  Picture canvas = new Picture("/Users/raineepei/Downloads/Unit16Students/Unit16-Assignments-pixLab/images/640x480.jpg");
+	  canvas.myCollage();
+	  canvas.explore();
+  }
+  
+  /** Method to test edgeDetection */
+  public static void testEdgeDetection()
+  {
+    Picture swan = new Picture("/Users/raineepei/Downloads/Unit16Students/Unit16-Assignments-pixLab/images/swan.jpg");
+    swan.edgeDetection(10);
+    swan.explore();
+  }
+  
+  public static void testEdgeDetection2() {
+	  Picture swan = new Picture("/Users/raineepei/Downloads/Unit16Students/Unit16-Assignments-pixLab/images/swan.jpg");
+	  swan.explore();
+	  swan.edgeDetection2(17);
+	  swan.explore();
+  }
+  
+  /** Main method for testing.  Every class can have a main
+    * method in Java */
+  public static void main(String[] args)
+  {
+    // uncomment a call here to run a test
+    // and comment out the ones you don't want
+    // to run
+    //testZeroBlue();
+    //testKeepOnlyBlue();
+    //testKeepOnlyRed();
+    //testKeepOnlyGreen();
+    //testNegate();
+    //testGrayscale();
+    //testFixUnderwater();
+    //testMirrorVertical();
+	  //testWater();
+	  //testMirrorArms();
+	  //testGull();
+    //testMirrorTemple();
+    //testMirrorArms();
+    //testMirrorGull();
+    //testMirrorDiagonal();
+    //testCollage();
+    testCopy();
+    //testEdgeDetection();
+    //testEdgeDetection2();
+    //testChromakey();
+    //testEncodeAndDecode();
+    //testGetCountRedOverValue(250);
+    //testSetRedToHalfValueInTopHalf();
+    //testClearBlueOverValue(200);
+    //testGetAverageForColumn(0);
+  }
+}
